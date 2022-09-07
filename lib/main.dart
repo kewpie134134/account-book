4255e92f1d7e74b72a6e907ca7ebac128e879db8
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2022-09-07 19:30:04 +0900

    [add] Firebase の初期設定を追記

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
+import 'package:account_book/config/config.dart';
 import 'package:account_book/pages/household_account_book_detail.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+// Firebase のシークレット情報を取得
+final Configurations configurations = Configurations();
+
+void main() async {
+  // Flutter Engine を利用する際の宣言（Firebase を利用する際は必要）
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: configurations.apiKey,
+      authDomain: configurations.authDomain,
+      projectId: configurations.projectId,
+      messagingSenderId: configurations.messagingSenderId,
+      appId: configurations.appId,
+      measurementId: configurations.measurementId,
+    ),
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
