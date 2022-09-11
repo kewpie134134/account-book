08ce29ef77374b615f62ebebf2dec8264f7eb1f8
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2022-09-12 00:02:52 +0900

    [update] Firestore から値をきれいに持ってこれるように修正

--- lib/components/drawer_menu.dart
+++ lib/components/drawer_menu.dart
@@ -1,47 +1,54 @@
 import 'package:account_book/app_root.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:account_book/stores/household_account_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class DrawerSelectYear extends StatelessWidget {
+class DrawerSelectYear extends ConsumerWidget {
   const DrawerSelectYear({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<QuerySnapshot>(
-        future: FirebaseFirestore.instance
-            .collection("users")
-            .doc("user1")
-            .collection("years")
-            .get(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final List<DocumentSnapshot> documents = snapshot.data!.docs;
-            final List<Widget> resList = [];
-            resList.add(const UserAccountsDrawerHeader(
-              accountName: Text("user1"),
-              accountEmail: Text("example@example.com"),
-            ));
-            for (final document in documents) {
-              resList.add(ListTile(
-                title: Text(document.id),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).push<dynamic>(
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return const AppRoute();
-                      },
-                    ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final years = ref.watch(householdAccountYearProvider);
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          const UserAccountsDrawerHeader(
+            accountName: Text("user1"),
+            accountEmail: Text("example@example.com"),
+          ),
+          years.when(
+            data: (data) {
+              return Column(
+                children: data.map((doc) {
+                  return ListTile(
+                    title: Text(doc),
+                    onTap: () {
+                      // ここで Riverpod に値を渡したい
+                      Navigator.of(context).pop();
+                      Navigator.of(context).push<dynamic>(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const AppRoute();
+                          },
+                        ),
+                      );
+                    },
                   );
-                },
-              ));
-            }
-            return Drawer(child: ListView(children: resList));
-          } else if (snapshot.hasError) {
-            return const Text("通信エラーが発生しました");
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        });
+                }).toList(),
+              );
+            },
+            error: (error, stackTrace) {
+              return Text(
+                error.toString(),
+                style: const TextStyle(fontSize: 24),
+              );
+            },
+            loading: () {
+              return const Center(child: CircularProgressIndicator());
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/stores/household_account_data.dart
+++ lib/stores/household_account_data.dart
@@ -17,3 +17,17 @@ final householdAccountDataProvider = StreamProvider((ref) {
   });
   return stream;
 });
+
+final FutureProvider<List<String>> householdAccountYearProvider =
+    FutureProvider((ref) {
+  final collection = FirebaseFirestore.instance
+      .collection("users")
+      .doc("user1")
+      .collection("years");
+
+  return collection.get().then((snapshot) {
+    return snapshot.docs.map((doc) {
+      return doc.id;
+    }).toList();
+  });
+});
